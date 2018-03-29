e49bbc158ee575d1f060b3cfaabc1e4b48e7e66b
Jianfeng Zhang <swordfeng123@gmail.com>
2018-03-29 22:55:34 +0800

    all use CUDA_PATH

--- env.sh
+++ env.sh
@@ -2,5 +2,5 @@ export ONVM_HOME=$PWD
 export RTE_SDK="$ONVM_HOME/dpdk"
 export RTE_TARGET=x86_64-native-linuxapp-gcc
 export ONVM_NUM_HUGEPAGES=$(cat /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages)
-export CUDA_SDK=$(dirname $(dirname $(which nvcc)))
+export CUDA_PATH=$(dirname $(dirname $(which nvcc)))
 
