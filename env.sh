1a87f978f5a2469a32bf0a797a16663a209ae7c5
Jianfeng Zhang <swordfeng123@gmail.com>
2018-03-29 17:26:14 +0800

    add env shell file

---
+++ env.sh
@@ -0,0 +1,6 @@
+export ONVM_HOME=$PWD
+export RTE_SDK="$ONVM_HOME/dpdk"
+export RTE_TARGET=x86_64-native-linuxapp-gcc
+export ONVM_NUM_HUGEPAGES=$(cat /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages)
+export CUDA_SDK=$(dirname $(dirname $(which nvcc)))
+
