816029aac12e6c8ccb385d4b48c804620cacb9d0
Jianfeng Zhang <swordfeng123@gmail.com>
2018-03-29 23:14:25 +0800

    dpdk build script install and detect cpus

--- build_dpdk.sh
+++ build_dpdk.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+ncpu=$(grep -c '^processor' /proc/cpuinfo)
+
 pushd "$(dirname $0)"
 source env.sh
 cd dpdk
-make -j4 config T=$RTE_TARGET
-make -j4 T=$RTE_TARGET
+make -j$ncpu config T=$RTE_TARGET
+make -j$ncpu T=$RTE_TARGET
+make -j$ncpu install T=$RTE_TARGET
 popd
