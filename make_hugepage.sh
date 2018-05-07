76c7e5ce279085b933ed158b030e45c247ef67ac
Jianfeng Zhang <swordfeng123@gmail.com>
2018-05-07 15:44:35 +0800

    revert

--- make_hugepage.sh
+++ make_hugepage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo 512 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+echo 128 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
 echo 16 > /sys/kernel/mm/hugepages/hugepages-1048576kB/nr_hugepages
