39240a802bdcfe28feb96ae1c77cb0a19e167d10
Jianfeng Zhang <swordfeng123@gmail.com>
2018-05-07 15:54:48 +0800

    tryfix

--- onvm/onvm_mgr/onvm_init.h
+++ onvm/onvm_mgr/onvm_init.h
@@ -78,7 +78,7 @@
 #if defined(UNCO_SHARE_GPU) || defined(FAIR_SHARE_GPU)
 	#define MBUFS_PER_CLIENT (65000)
 #else
-	#define MBUFS_PER_CLIENT (8192)
+	#define MBUFS_PER_CLIENT (2048)
 #endif
 //#define MBUFS_PER_CLIENT (MAX_BATCH_SIZE * NUM_BATCH_BUF)
 #define MBUFS_PER_PORT (4 * 512 * 2)
