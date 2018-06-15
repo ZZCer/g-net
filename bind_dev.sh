e98e9489b99b2ea6388b87fe118c1a5927b446de
swordfeng <swordfeng123@gmail.com>
2018-06-15 20:08:47 +0800

    update dev name

--- bind_dev.sh
+++ bind_dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo modprobe uio
 sudo insmod dpdk/build/kmod/igb_uio.ko
-sudo ip link set p1p2 down
-sudo dpdk/usertools/dpdk-devbind.py -b igb_uio p1p2
+sudo ip link set enp2s0f1 down
+sudo dpdk/usertools/dpdk-devbind.py -b igb_uio enp2s0f1
 
